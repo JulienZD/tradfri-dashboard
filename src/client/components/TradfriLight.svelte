c71fd18cf804a2e02a24483c324c0d1ac0a8895b
Julien <jzapata@live.nl>
2021-06-24 21:58:16 +0200

    feat(client): change dark mode accent to blue instead of green

--- src/client/components/TradfriLight.svelte
+++ src/client/components/TradfriLight.svelte
@@ -7,7 +7,6 @@
   import RgbControls from './RGBControls.svelte';
   import { lightsStore } from '../stores/tradfri';
   import { tick } from 'svelte';
-  import { theme } from '../stores/theme';
 
   export let light: TradfriLightInfo;
 
@@ -36,7 +35,7 @@
 <div class="flex flex-col my-4 first:mt-0 last:mb-0">
   <div class="flex justify-between mb-3">
     <h2 class="font-medium text-lg">{light.name}</h2>
-    <Toggle hideLabel toggledColor={$theme === 'dark' ? '#10b981' : '#3B82F6'} toggled={light.isOn} on:click={toggle} />
+    <Toggle hideLabel toggledColor="#3B82F6" toggled={light.isOn} on:click={toggle} />
   </div>
 
   <RangeSlider float suffix="%" on:stop={setBrightness} values={[light.brightness]} />
@@ -53,13 +52,6 @@
     --range-handle-inactive: #0b60ea; /* inactive handle color */
     --range-handle: #1d49db; /* non-focussed handle color */
     --range-handle-focus: #0643b3; /* focussed handle color */
-  }
-
-  :global(html[class='dark']) {
-    --range-slider: #10b981; /* slider main background color */
-    --range-handle-inactive: #047857; /* inactive handle color */
-    --range-handle: #34d399; /* non-focussed handle color */
-    --range-handle-focus: #059669; /* focussed handle color */
   }
 
   :global(.rangeSlider) {
