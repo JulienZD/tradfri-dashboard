230fcc4753a285d692123f95de198a5d00c1f20c
Julien <jzapata@live.nl>
2021-06-24 21:52:51 +0200

    feat(client): display theme icon based on current theme

--- src/client/components/ThemeSwitcher.svelte
+++ src/client/components/ThemeSwitcher.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
   import { theme } from '../stores/theme';
+  import { SunIcon, MoonIcon } from 'svelte-feather-icons';
 </script>
 
 <button
-  class="float-right py-1 px-2 rounded dark:hover:bg-green-500 dark:hover:text-gray-900 duration-300"
-  on:click={theme.swap}>{$theme === 'dark' ? 'Light' : 'Dark'} Mode</button
+  class="group float-right border-none py-1 px-2 rounded focus:outline-none focus-visible:ring focus-visible:ring-blue-500"
+  on:click={theme.swap}
 >
+  {#if $theme === 'dark'}
+    <SunIcon class="group-hover:fill-current" size="24" />
+  {:else}
+    <MoonIcon size="24" class="group-hover:fill-current" />
+  {/if}
+</button>
